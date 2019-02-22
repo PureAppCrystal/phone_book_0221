2ad98fc7299c70404746c8b71d0b013d7decc110
PureAppCrystal <purecrystar@gmail.com>
2019-02-22 23:04:45 +0900

    phone 등록기능 구현

--- src/containers/PhoneBookContainer.js
+++ src/containers/PhoneBookContainer.js
@@ -10,20 +10,37 @@ class PhoneBookContainer extends Component {
 
     }
 
+    handleChange = (e) => {
+        console.log("====== phonebook/Change ======")
+        const { PhoneActions } = this.props
+        PhoneActions.change({[e.target.name]: e.target.value})
+    }
+
     handleInsert = () => {
-        console.log(" ====== handleInsert ======")
-        const {id, name, number, PhoneActions } = this.props
+        console.log("====== phonebook/Insert ======")
+        const { id, name, number } = this.props
+        const { PhoneActions } = this.props
+
         PhoneActions.insert({id, name, number});
+    }
+
+    handleRemove = (e) => {
+        console.log("====== phonebook/Remove ======")
+        const { PhoneActions } = this.props
+        PhoneActions.remove(e)
     }
 
     render() {
         const {id, name, number } = this.props;
-        const { handleInsert } = this.props;
+        const { handleInsert, handleRemove } = this.props;
         return (
             <div>
                 PhoneBookContainer
                 {id}
                 <button onClick={this.handleInsert} >등록 테스트 </button>
+                <button onClick={this.handleRemove} >삭제 테스트 </button>
+                <input name="name" placeholder="name" onChange={this.handleChange}/>
+                <input name="number" placeholder="number" onChange={this.handleChange}/>
             </div>
             
         )
@@ -33,12 +50,12 @@ class PhoneBookContainer extends Component {
 
 export default connect(
     (state) => ({
-        id: state.phonebook.id,
-        name: state.phonebook.name,
-        number: state.phonebook.number,
-        phoneList: state.phonebook.phoneList,
-        searchState : state.phonebook.searchState,
-        searchList: state.phonebook.searchList
+        id: state.phonebook.get('id'),
+        name: state.phonebook.get('name'),
+        number: state.phonebook.get('number'),
+        phoneList: state.phonebook.get('phoneList'),
+        searchState : state.phonebook.get('searchState'),
+        searchList: state.phonebook.get('searchList')
     }),
     (dispatch) => ({
         PhoneActions: bindActionCreators(phoneActions, dispatch)
--- src/modules/phonebook.js
+++ src/modules/phonebook.js
@@ -1,33 +1,49 @@
 import { handleActions, createAction } from 'redux-actions';
+import { Map, List, FromJS} from 'immutable';
 
-
-
+const CHANGE = "CHANGE"
 const INSERT = "INSERT"
 const REMOVE = "REMOVE"
 
+export const change = createAction(CHANGE);
 export const insert = createAction(INSERT);
 export const remove = createAction(REMOVE);
 
 
-const initialState = {
+const getId = (state) => {
+    let nowId = state.get('id');
+    return  ++nowId;
+}
+
+const initialState = Map({
     id: 0,
     name: '',
     number: '',
-    phoneList: [],
-    searchList: [],
+    phoneList: List([]),
+    searchList: List([]),
     searchState: false
-}
+})
 
 export default handleActions({
+    [CHANGE]: (state, action) => {
+        //
+        const param = action.payload;
+        const newState = state.merge(param)
+        return newState;
+    },
     [INSERT]: (state, action) => {
         //
-        console.log("====== handleAction/INSERT ======")
-        console.log("state : " , state)
-        console.log("action : ", action)
-        return state
+        const {name, number } = action.payload;
+        const newId = getId(state)
+        const newState = state.set('phoneList', state.get('phoneList').push(Map({
+            id: newId,
+            name: name,
+            number: number
+        }))).set('id', newId)
+        return newState;
     },
     [REMOVE]: (state, action) => {
         //
-        return state
+        return state;
     }
 }, initialState)
